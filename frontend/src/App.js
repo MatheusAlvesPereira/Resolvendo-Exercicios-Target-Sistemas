2fb305d3dce779179ae02758654fe189f1e05b80
Matheus Alves <matheusalvessilvabr@gmail.com>
2023-03-06 21:35:29 -0300

    home page in react.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import Home from "./components/home";
+import './styles/style.css';
+
+function App() {
+  return (
+    <>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/home.jsx
@@ -0,0 +1,29 @@
+function Home() {
+    return (
+    <>
+        <header className="header">
+            <nav className="nav">
+                <ul className="nav__list">
+                    <li className="nav__item">
+                        <a href="#card__price" className="nav__link">Exercicio2</a>
+                    </li>
+
+                    <li className="nav__item">
+                        <a href="#card__price" className="nav__link">Exercicio3</a>
+                    </li>
+
+                    <li className="nav__item">
+                        <a href="#about" className="nav__link">Exercicio4</a>
+                    </li>
+
+                    <li className="nav__item">
+                        <a href="#card__price" className="nav__link">Exercicio5</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>   
+    </>
+    );
+  }
+  
+  export default Home;
